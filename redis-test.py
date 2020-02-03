b31755591cec4ca759cff782664b31065da98eef
Shah nazari <749453@cognizant.com>
2020-02-03 13:00:11 +0100

    clean up nginx conf

--- redis-test.py
+++ redis-test.py
@@ -1,7 +1,10 @@
 import redis
 
-r = redis.StrictRedis(host='localhost', port=6379, db=4)
-print ("set key1 123")
-print (r.set('key1', '123'))
-print ("get key1")
-print(r.get('key1'))
+r = redis.StrictRedis(host='127.0.0.1', port=6379, db=0)
+#print ("set key1 123")
+#print (r.set('key1', '123'))
+print ("get keys")
+for k in r.keys():
+    print (k)
+
+
