61216c160e02d2015d9a4f3bb573171ba0648467
Shah nazari <749453@cognizant.com>
2020-02-03 13:01:38 +0100

    python redis test file added

--- redis-test.py
+++ redis-test.py
@@ -2,7 +2,7 @@ import redis
 
 r = redis.StrictRedis(host='127.0.0.1', port=6379, db=0)
 #print ("set key1 123")
-#print (r.set('key1', '123'))
+#list all keys
 print ("get keys")
 for k in r.keys():
     print (k)
